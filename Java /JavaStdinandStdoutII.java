0dbd4ad88f202afb2f23ccf9e9d613d492afc136
Aditya Agarwal <136118380+Adityaagar2003@users.noreply.github.com>
2024-05-23 19:28:37 +0530

    Create JavaStdinandStdoutII.java

---
+++ Java /JavaStdinandStdoutII.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        Double c=sc.nextDouble();
+        sc.nextLine();
+        String s=sc.nextLine();
+        System.out.println("String: "+s);
+        System.out.println("Double: "+c);
+        System.out.println("Int: "+a);
+    }
+}
